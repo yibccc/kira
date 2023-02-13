4d24ef53b5d56642eb8a19cc7af6d2390ada4a70
Aromazhangxinyi <2507300798@qq.com>
2023-02-14 00:43:20 +0800

    新增周程表和日程表（未能获取后端传来的数据）

--- src/main/resources/static/api/schedule.js
+++ src/main/resources/static/api/schedule.js
@@ -1,6 +1,30 @@
+//获取职位列表
+function getJobList (params) {
+    return $axios({
+        url:'/jobs/list',
+        method:'get',
+        params
+    })
+}
+
+//获取门店列表
 function getStoreList(params){
     return $axios({
-        url:'/stores/lift',
+        url:'/stores/list',
+        method:'get',
+        params
+    })
+}
+function getEmployeeList(params){
+    return $axios({
+        url:'/employees/page',
+        method:'get',
+        params
+    })
+}
+function getSelectedInfo(params){
+    return $axios({
+        url:'/paiban/list',
         method:'get',
         params
     })
