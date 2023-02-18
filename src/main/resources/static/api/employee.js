669f608b96d24659a8ab0d3033cc05c5f921c1bf
Aromazhangxinyi <2507300798@qq.com>
2023-02-18 19:24:43 +0800

    1.修改了某些管理员无法删除等功能 2.增加认证员工登录和管理员登录（员工登录后只可查看排班表，不能进行任何其他操作）

--- src/main/resources/static/api/employee.js
+++ src/main/resources/static/api/employee.js
@@ -75,3 +75,24 @@ function getFlavorById(id){
     method:'get'
   })
 }
+
+function loginApi(data) {
+  return $axios({
+    'url': '/employees/login',
+    'method': 'post',
+    data
+  })
+}
+function logoutApi(){
+  return $axios({
+    'url': '/employees/logout',
+    'method': 'post',
+  })
+}
+function editPwd(params){
+  return $axios({
+    url:'/employees',
+    method:'put',
+    data:{...params}
+  })
+}
